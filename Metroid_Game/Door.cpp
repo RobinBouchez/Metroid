0a7b67532e4d3d054f2e17ac82abe520ece3f0c4
Robin bouchez <75143514+RobinBouchez@users.noreply.github.com>
2022-08-23 04:43:57 +0200

    Create Door

--- Metroid_Game/Door.cpp
+++ Metroid_Game/Door.cpp
@@ -2,24 +2,67 @@
 #include "Door.h"
 #include "Texture.h"
 #include "TextureManager.h"
+#include "Camera.h"
+#include "Player.h"
+#include "utils.h"
+#include "Animation.h"
 
-Door::Door() : GameObject()
+Door::Door(const Point2f& position) : GameObject()
 	, m_pTexture{ nullptr }
 	, m_IsClosed{ true }
 {
+	m_Position = position;
 	m_pTexture = TextureManager::GetInstance().CreateTexture("Door");
+	m_Width = m_pTexture->GetWidth() / 3.f; 
+	m_Height = m_pTexture->GetHeight();
+}
+
+Door::~Door()
+{
+	delete m_pPlayer;
+	m_pPlayer = nullptr;
+
+	delete m_pAnimation;
+	m_pAnimation = nullptr;
+
+	delete m_pCamera;
+	m_pCamera = nullptr;
 }
 
 void Door::Draw() const
 {
-	m_pTexture->Draw();
+	if (m_IsClosed)
+	{
+		Rectf destRect;
+		destRect.left = m_Position.x;
+		destRect.bottom = m_Position.y;
+		destRect.width = m_Width;
+		destRect.height = m_Height;
+
+		Rectf srcRect;
+		srcRect.left = m_pAnimation->m_AnimationFrame * m_Width;
+		srcRect.bottom = 0.f;
+		srcRect.width = m_Width;
+		srcRect.height = m_Height;
+
+		m_pTexture->Draw(destRect, srcRect);
+	}
 }
 
 void Door::Update(float elapsedSec)
 {
+	if (utils::IsOverlapping(m_pPlayer->m_Shape, this->GetBoundaries()))
+	{
+		if (!m_IsClosed)
+		{
+			m_pCamera->Transition(); // to next level
+			Open();
+		}
+	}
 }
 
 bool Door::Open()
 {
-	return false;
+	m_IsClosed = false;
+	return m_IsClosed;
 }
--- Metroid_Game/Door.h
+++ Metroid_Game/Door.h
@@ -2,12 +2,15 @@
 #include "GameObject.h"
 
 class Texture;
+class Animation;
+class Camera;
+class Player;
 
 class Door final : public GameObject
 {
 public:
-	Door();
-	virtual ~Door() = default;
+	Door(const Point2f& position);
+	virtual ~Door();
 
 	virtual void Draw() const override;
 	virtual void Update(float elapsedSec) override;
@@ -16,6 +19,10 @@ public:
 
 private:
 	Texture* m_pTexture;
+	Animation* m_pAnimation;
+	Camera* m_pCamera;
+	Player* m_pPlayer;
+
 	bool m_IsClosed;
 };
 
